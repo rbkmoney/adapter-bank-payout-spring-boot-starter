508d8ea66315d4708b476ad1187b6c4ae54e390e
vbanin <vitaxa93gamebox@gmal.com>
2019-06-20 17:45:16 +0300

    - fix state mapping
    - exclude pan field

--- src/main/java/com/rbkmoney/adapter/bank/payout/spring/boot/starter/model/EntryStateModel.java
+++ src/main/java/com/rbkmoney/adapter/bank/payout/spring/boot/starter/model/EntryStateModel.java
@@ -1,6 +1,7 @@
 package com.rbkmoney.adapter.bank.payout.spring.boot.starter.model;
 
 import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringExclude;
 
 import java.util.Map;
 
@@ -9,6 +10,7 @@ public class EntryStateModel {
     private String withdrawalId;
     private Long amount;
     private String currencyCode;
+    @ToStringExclude
     private String pan;
     private Map<String, String> options;
 
